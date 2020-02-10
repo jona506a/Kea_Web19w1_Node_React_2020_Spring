79c8bb70220ad147c355780fc50f052a530aa20a
anders <anderslatif@gmail.com>
2020-02-10 13:51:52 +0100

    Did conditional rendering with ternary statements

--- 2._First_React_Project/src/App.js
+++ 2._First_React_Project/src/App.js
@@ -2,20 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import ProfilePage from './components/profile_page/ProfilePage';
 import BusPage from './components/bus_page/BusPage';
+import Profile from './components/profile_page/Profile';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navpage: 'buspage'
+    };
+  }
 
-  onNavButtonClicked = (text) => {
-    console.log(text);
+  onNavButtonClicked = (navpage) => {
+    this.setState({ navpage });
   };
 
   render() {
+    const { navpage } = this.state;
+
+    console.log("this is the nav value:", navpage);
+
     return (
       <div className="App">
-        <button onClick={() => console.log("it worked")}>Bus Page</button>
-        <button onClick={() => console.log("also works")}>Profile Page</button>
-        <BusPage />
-        <ProfilePage />
+        <button onClick={() => this.onNavButtonClicked('buspage')}>Bus Page</button>
+        <button onClick={() => this.onNavButtonClicked('profilepage')}>Profile Page</button>
+        { navpage === 'buspage' ? 
+          <BusPage /> 
+          :
+          <ProfilePage />
+        }
       </div>
     );
   }
