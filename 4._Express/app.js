3dd48bc70e008ff21feaf0c227d8ac359f06d3ab
anders <anderslatif@gmail.com>
2020-03-09 13:51:47 +0100

    Finished the CRUD API

--- 4._Express/app.js
+++ 4._Express/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(express.json());
+
 app.get("/", (req, res) => {
     return res.send({ status: "The API is up and running" });
 });
@@ -27,6 +33,40 @@ app.get("/time", (req, res) => {
 app.get("/search", (req, res) => {
     console.log(req.query);
     return res.send({ });
+});
+
+/* CRUD API */
+
+let cars = [{ id: 1, type: "SUV" }, { id: 2, brand: "Volvo" }, { id: 3 }];
+let currentId = 3;
+
+app.get("/cars", (req, res) => {
+    return res.send({ response: cars });
+});
+
+app.get("/cars/:id", (req, res) => {
+    const foundCar = cars.find(car => car.id === Number(req.params.id));
+    return res.send({ response: foundCar });
+});
+
+app.post("/cars/", (req, res) => {
+    const newCar = req.body;
+    newCar.id = ++currentId;
+    cars.push(newCar);
+    return res.send({ response: newCar });
+});
+
+app.put("/cars/:id", (req, res) => {
+    const foundIndex = cars.findIndex(car => car.id === Number(req.params.id));
+    const newCar = { ...cars[foundIndex], ...req.body };
+    cars[foundIndex] = newCar;
+
+    return res.send({ response: newCar });
+});
+
+app.delete("/cars/:id", (req, res) => {
+    cars = cars.filter(car => car.id !== Number(req.params.id));
+    return res.send({ response: cars });
 });
 
 app.listen(9090, (error) => {
