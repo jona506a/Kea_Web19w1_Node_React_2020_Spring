d4bdbfc4fca8bc56cfcdaa6def121e8610ebf6e3
anders <anderslatif@gmail.com>
2020-03-23 09:46:54 +0100

    Setup basic knex and added dbcredentials to .gitignore

---
+++ 5._Auth/server/knexfile.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    }
+  }
+
+};
