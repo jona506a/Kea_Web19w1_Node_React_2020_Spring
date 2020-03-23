0fd5fba7d64d87bc22ab950e4b428ad80e16a82b
anders <anderslatif@gmail.com>
2020-03-23 13:29:57 +0100

    Created the first seed

--- 5._Auth/server/migrations/20200323100557_initial_migration.js
+++ 5._Auth/server/migrations/20200323100557_initial_migration.js
@@ -1,13 +1,25 @@
-
 exports.up = function(knex) {
     return knex.schema
         .createTable('users', (table) => {
             table.increments('id');
             table.string('username');
+            table.string('first_name');
+            table.string('last_name');
+            table.timestamp('created_at').defaultTo(knex.fn.now());
+        })
+        .createTable('addresses', (table) => {
+            table.increments('id');
+            table.string('address_1');
+            table.string('address_2');
+            table.string('postal_code');
+            table.string('city');
+            table.integer('user_id').unsigned().notNullable();
+            table.foreign('user_id').references('users.id');
         });
 };
 
 exports.down = function(knex) {
     return knex.schema
+        .dropTableIfExists('addresses')
         .dropTableIfExists('users');
 };
